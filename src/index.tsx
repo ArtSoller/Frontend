04469ce6c8e7006e0e0ecbd646c927adefc4a1d0
ArtSoller <art-sav2002@yandex.ru>
2024-05-30 23:18:02 +0700

    Mil 2 - Task 4 - src/* - Modify src/index.tsx and recreate src/react-app-env.d.ts and src/reportWebVitals.ts files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
+import reportWebVitals from "./reportWebVitals";
+
 import {Provider} from "react-redux";
-import { createRoot } from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 
+import App from './components/App/App';
+import {store} from "./store/store";
+import AppRoutes from "./components/App/AppRoutes";
 
-import App from "./components/App/App";
-import {store} from "./store";
-
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>
 );
+reportWebVitals();
