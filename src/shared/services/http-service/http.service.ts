a4d3eb80541d389ae0e905a7c114c942c34a97b8
ArtSoller <art-sav2002@yandex.ru>
2024-07-04 20:23:07 +0700

    Mil 2 - Task 12 - src/shared/services - Add logging service and http-service with tostify

---
+++ src/shared/services/http-service/http.service.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError } from "axios"
+// import { logger } from "../logging-service"
+import { toast } from "react-toastify"
+axios.interceptors.response.use(
+    res => res,
+    function (error: AxiosError) {
+        const expectedErrors =
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500
+
+        if (!expectedErrors) {
+            toast("Unexpected Error")
+            // logger.log(error)
+        }
+        return Promise.reject(error)
+    }
+)
+
+export const httpService = {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+}
