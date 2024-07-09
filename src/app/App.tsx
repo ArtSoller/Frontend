b7a5ccae275bd6239cbbbc49980c87524ddac6c0
ArtSoller <art-sav2002@yandex.ru>
2024-07-10 00:28:48 +0700

    Mil 2 - Task 12 - src/app/App.tsx - Change add then change back file

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+import { Pages } from "../pages"
+import "./styles/index.css"
+
+
+function App() {
+    return (
+        <>
+            <Pages />
+            <ToastContainer />
+        </>
+    )
+}
+
+export default App
