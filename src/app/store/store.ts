dc7d9556bb7ea3e3b46be8deb5fcd306b07202c2
ArtSoller <art-sav2002@yandex.ru>
2024-07-10 00:48:11 +0700

    Mil 2 - Task 15 - src/app/store/store.ts - Add UserSlice import

---
+++ src/app/store/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+import SignUpModalReducer from "../../features/SignUpButton/model/SignUpModalSlice.ts"
+import SignInModalReducer from "../../features/SignInButton/model/SignInModalSlice.ts"
+import UserReducer from "../../entities/user/model/UserSlice.ts"
+
+export const store = configureStore({
+    reducer: {
+        SignUpModal: SignUpModalReducer,
+        SignInModal: SignInModalReducer,
+        user: UserReducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
