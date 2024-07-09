47762bff91e850f1b8d0a6f37cfcd5d13ce092fb
ArtSoller <art-sav2002@yandex.ru>
2024-07-10 00:55:39 +0700

    Mil 2 - Task 15 - src/index.tsx - Configure Redux store

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {BrowserRouter} from "react-router-dom"
+
+
+import App from "./app/App.tsx"
+import { Provider } from "react-redux"
+import { store } from "./app/store/store.ts"
+// import { logger } from "./shared/services/logging-service"
+
+// logger.init()
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    </BrowserRouter>
+)
