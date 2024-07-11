7b5e4fbb4f9c8956075cd523b15a008af0e43de9
ArtSoller <art-sav2002@yandex.ru>
2024-07-11 23:02:50 +0700

    Mil 2 - Task 16 - src/widgets/LandingPageNavBar/LandingPageNavBar.tsx - Create description for NavbarLinks

--- src/widgets/LandingPageNavBar/LandingPageNavBar.tsx
+++ src/widgets/LandingPageNavBar/LandingPageNavBar.tsx
@@ -1,33 +1,49 @@
+import { useLocation } from "react-router-dom"
+
 import { SignUpButton } from "../../features/SignUpButton"
 import { SignInButton } from "../../features/SignInButton"
+import { NavBarLinks } from "../../features/NavBarFeatures/NavBarLinks"
 
-// TODO: Do something with it.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import Logo from "../../shared/media/Logo.svg"
 
+interface NavbarLink {
+    name: string
+    path: string
+    isSelected: boolean
+}
+
 export const LandingPageNavBar = () => {
-    const linkClasses: string =
-        "text-viat-primary font-viat-body hover:text-viat-accent font-bold"
+    const location = useLocation()
+    const navbarLinksConfig: NavbarLink[] = [
+        {
+            name: "CONVERT",
+            path: "/"
+        },
+        {
+            name: "COMPARISON",
+            path: "/features"
+        },
+        {
+            name: "ANALYTICS",
+            path: "/tips"
+        },
+    ].map(link => ({
+        ...link,
+        isSelected: location.pathname === link.path
+    }))
+
     return (
         <header className='relative h-20'>
             <div className='container mx-auto px-24 h-full'>
                 <div className='flex items-center justify-between h-full'>
                     <div className='flex items-center'>
-                        <img src="src/shared/media/Logo.svg" alt='Capital Compass Logo' className='h-10 w-auto' />
+                        <img
+                            src={Logo.toString()}
+                            alt='Capital Compass Logo'
+                            className='h-10 w-auto'
+                        />
                     </div>
-                    <nav className='flex space-x-8'>
-                        {/*TODO: Make actual links*/}
-                        <a href='' className={linkClasses}>
-                            Convert
-                        </a>
-                        <a href='' className={linkClasses}>
-                            Comparison
-                        </a>
-                        <a href='' className={linkClasses}>
-                            Analytics
-                        </a>
-                    </nav>
+                    <NavBarLinks linksConfig={navbarLinksConfig}/>
                     <div className='flex space-x-4'>
                         <SignUpButton />
                         <SignInButton />
