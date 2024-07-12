89593bdbf1a3b334ad51b1d14299d786da580c05
ArtSoller <art-sav2002@yandex.ru>
2024-07-12 23:46:23 +0700

    Mil 2 - Task 16 - src/shared/UserListFeatures/EmptyListPicture/* && src/shared/media/* - Create feature for picture of empty list and add the picture itself

---
+++ src/features/UserListFeatures/EmptyListPicture/EmptyListPicture.tsx
@@ -0,0 +1,13 @@
+import emptyListPicture from "../../../shared/media/Empty.svg"
+
+export const EmptyListPicture = () => {
+    return (
+        <div className='flex justify-center items-center h-full col-span-6 col-start-4'>
+            <img
+                src={emptyListPicture.toString()}
+                alt='Your list is empty'
+                className='mx-auto'
+            />
+        </div>
+    )
+}
