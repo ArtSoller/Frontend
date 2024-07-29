887fc8d8b6e776787d7896272b6989d8597e1a3a
Артем Саляев <art-sav2002@yandex.ru>
2024-07-29 19:41:09 +0700

    Mil 2 - Task 18 - src/features/UserListFeatures/RuleItem/* - Add styles and change position of icons on RuleItem

--- src/features/UserListFeatures/RuleItem/RuleItem.tsx
+++ src/features/UserListFeatures/RuleItem/RuleItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Tooltip from '@mui/material/Tooltip';
 import Popover from '@mui/material/Popover';
@@ -74,65 +74,86 @@ export const RuleItem = ({ rule, onDelete, onActivate, onUpdate }: RuleProps) =>
 
     const open = Boolean(anchorEl);
     const id = open ? 'info-popover' : undefined;
+
     return (
         <div>
             <div className='bg-viat-secondary shadow-md rounded-md p-4 mt-4'>
-                <div className='flex items-center space-x-2'>
-                    <img src={AlertIcon.toString()} alt='User Icon' className='h-10 w-10'/>
-                    <div className='flex flex-col space-y-1'>
-                        <div className='font-viat-small text-viat-size-body text-viat-text'>
-                            <div>Currency: {rule.currency.name}</div>
-                            <div>Upper Rule Rate: {rule.upper_alert_rate}</div>
-                            <div>Lower Rule Rate: {rule.lower_alert_rate}</div>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center space-x-2'>
+                        <img src={AlertIcon.toString()} alt='User Icon' className='h-10 w-10'/>
+                        <div className='flex flex-col space-y-1'>
+                            <div className='font-viat-small text-viat-size-body text-viat-text'>
+                                <div>Currency: {rule.currency.name}</div>
+                                <div>Upper Rule Rate: {rule.upper_alert_rate}</div>
+                                <div>Lower Rule Rate: {rule.lower_alert_rate}</div>
+                            </div>
                         </div>
                     </div>
-                    <Tooltip title={
-                        <span>
-                            {rule.rule_status ? "Deactivate Rule" : "Activate Rule"}
-                            <IconButton size="small" onClick={handleInfoClick}>
-                                <InfoIcon fontSize="inherit" />
-                            </IconButton>
-                        </span>
-                    } placement="top">
-                        <span>
-                            {rule.rule_status ? (
-                                <CheckCircleIcon onClick={handleActivate} className='cursor-pointer icon-default icon-hover' />
-                            ) : (
-                                <RadioButtonUncheckedIcon onClick={handleActivate} className='cursor-pointer icon-default icon-hover' />
-                            )}
-                        </span>
-                    </Tooltip>
-                    <Popover
-                        id={id}
-                        open={open}
-                        anchorEl={anchorEl}
-                        onClose={handleInfoClose}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'center',
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'center',
-                        }}
-                    >
-                        <div style={{ padding: '10px' }}>
-                            Active rules track the currency and deactivate upon triggering, sending an email notification.
-                        </div>
-                    </Popover>
-                    <DeleteIcon onClick={handleDelete} className='cursor-pointer icon-default icon-hover' />
-                    <EditRuleButton ruleId={rule.id} />
-                    {isOpen && ruleId === rule.id && (
-                        <WorkspacePageModalView isOpen={isOpen} onClose={handleCloseModal}>
-                            <EditRuleModalFields
-                                initialCurrency={rule.currency}
-                                initialUpperExchangeRate={rule.upper_alert_rate.toString()}
-                                initialLowerExchangeRate={rule.lower_alert_rate.toString()}
-                                ruleId={ruleId}
-                            />
-                        </WorkspacePageModalView>
-                    )}
+                    <div className='flex space-x-2'>
+                        <Tooltip title={
+                            <span>
+                                {rule.rule_status ? "Deactivate Rule" : "Activate Rule"}
+                                <IconButton size="small" onClick={handleInfoClick}>
+                                    <InfoIcon fontSize="inherit" />
+                                </IconButton>
+                            </span>
+                        } placement="top">
+                            <span>
+                                <IconButton size="small" className='cursor-pointer interactable'>
+                                    {rule.rule_status ? (
+                                        <CheckCircleIcon onClick={handleActivate}
+                                                         className='cursor-pointer icon-default icon-hover interactable'/>
+                                    ) : (
+                                        <RadioButtonUncheckedIcon onClick={handleActivate}
+                                                                  className='cursor-pointer icon-default icon-hover interactable'/>
+                                    )}
+                                </IconButton>
+                            </span>
+                        </Tooltip>
+                        <Popover
+                            id={id}
+                            open={open}
+                            anchorEl={anchorEl}
+                            onClose={handleInfoClose}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <div style={{ padding: '10px' }}>
+                                Active rules track the currency and deactivate upon triggering, sending an email notification.
+                            </div>
+                        </Popover>
+                        <Tooltip title="Delete Rule" placement="top">
+                            <span>
+                                <IconButton size="small" onClick={handleDelete} className='cursor-pointer interactable'>
+                                    <DeleteIcon className='icon-default icon-hover' />
+                                </IconButton>
+                            </span>
+                        </Tooltip>
+                        <Tooltip title="Edit Rule" placement="top">
+                            <span>
+                                <IconButton size="small" className='cursor-pointer interactable'>
+                                    <EditRuleButton ruleId={rule.id}/>
+                                </IconButton>
+                            </span>
+                        </Tooltip>
+                    </div>
                 </div>
+                {isOpen && ruleId === rule.id && (
+                    <WorkspacePageModalView isOpen={isOpen} onClose={handleCloseModal}>
+                        <EditRuleModalFields
+                            initialCurrency={rule.currency}
+                            initialUpperExchangeRate={rule.upper_alert_rate.toString()}
+                            initialLowerExchangeRate={rule.lower_alert_rate.toString()}
+                            ruleId={ruleId}
+                        />
+                    </WorkspacePageModalView>
+                )}
             </div>
         </div>
     );
