797854263ce117cfd7153309ad1851129f253a5e
Артем Саляев <art-sav2002@yandex.ru>
2024-07-29 19:46:16 +0700

    Mil 2 - Task 18 - src/features/SideBarFeatures/ProfileFields/ProfileFields.tsx - Add working Log out button

--- src/features/SideBarFeatures/ProfileFields/ProfileFields.tsx
+++ src/features/SideBarFeatures/ProfileFields/ProfileFields.tsx
@@ -1,23 +1,33 @@
-import UserIcon from "../../../shared/media/Profile.svg"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import UserIcon from "../../../shared/media/Profile.svg";
+import { logout } from '../../../shared/services/auth'; // Импорт функции logout
 
-export const ProfileFields = () => {
+export const ProfileFields: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/'); // Перенаправляем пользователя на главную страницу после выхода
+    };
+
     return (
         <div className='bg-viat-secondary shadow-lg rounded-lg p-6 mt-8 border border-viat-primary'>
-            <div className='flex items-center space-x-4'>
-                <img src={UserIcon.toString()} alt='User Icon' className='h-12 w-12 rounded-full border-2 border-viat-primary' />
-                <div className='flex flex-col space-y-1'>
+            <div className='d-flex align-items-center space-x-4'>
+                <img src={UserIcon.toString()} alt='User Icon' className='h-12 w-12 rounded-circle border-2 border-viat-primary' />
+                <div className='d-flex flex-column space-y-1'>
                     <div className='font-viat-medium text-viat-size-body text-viat-text'>
                         {/*TODO: Make link and show username*/}
                         {"Username's Workspace"}
                     </div>
-                    <a
-                        href=''
-                        className='font-viat-medium text-viat-size-small text-viat-primary hover:text-viat-primary-dark'
+                    <button
+                        onClick={handleLogout}
+                        className='btn btn-primary mt-0'
                     >
-                        View Profile
-                    </a>
+                        Log out
+                    </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
